048aaa3c256b0c910f0085f18eee43ebb3a5d3db
Devada <doos4230@gmail.com>
2025-04-12 09:28:16 +0700

    chore: add sample test case

---
+++ tests/util.test.ts
@@ -0,0 +1,10 @@
+﻿import {test, expect} from "bun:test";
+import {Level} from "bug-me-sdk-ts";
+
+test("add function", () => {
+  expect(Level.Debug == "Debug").toBe(true);
+})
+
+test("add other function", () => {
+  expect(Level.Debug == "Debug").toBe(false);
+})
